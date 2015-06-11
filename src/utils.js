1626895bbe6598560b75455f249c495a6e9e8fd3
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-06-10 18:42:49 -0600

    Change debug logger to be synchronous.

--- src/utils.js
+++ src/utils.js
@@ -24,25 +24,7 @@ export function isGreyspace(string) {
     return stream.atEnd();
 }
 
-let stream = fs.createWriteStream("", {
-    fd: 3,
-    encoding: "utf8"
-});
-
-function handleError(error) {
-    if (error) {
-        if (error.code === "EBADF") {
-            stream = null;
-        } else {
-            throw error;
-        }
-    }
-}
-
-stream.on("error", handleError);
-
 export function log(...messages) {
-    if (!stream) return;
-
-    stream.write(messages.join("") + "\n", "utf8", handleError);
+    let data = messages.join(" ") + "\n";
+    fs.writeSync(3, data);
 }
