9246a44a065a046293492edeccafc376a9aa628f
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-06-11 17:44:23 -0600

    Create greyspace parser function to allow for splitting greyspace by type.

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,11 @@
 import fs from "fs";
 import InputStream from "./InputStream";
 
+const whitespaceRe = /\s+/;
+const shebangRe = /#!.*\n/;
+const lineCommentRe = /\/\/[^\n]*/;
+const blockCommentRe = /\/\*[\W\S]*?\*\//;
+
 export function isGreyspace(string) {
     if (string === "" ||
         string === " " ||
@@ -15,16 +20,42 @@ export function isGreyspace(string) {
     let stream = new InputStream(string);
 
     // Consume shebang
-    stream.consume(/#!.*\n/);
+    stream.consume(shebangRe);
 
     while (!stream.atEnd()) {
-        let consumed = stream.consume(/\s+/) ||
-            stream.consume(/\/\/[^\n]*/) ||
-            stream.consume(/\/\*[\W\S]*?\*\//);
+        let consumed = stream.consume(whitespaceRe) ||
+            stream.consume(lineCommentRe) ||
+            stream.consume(blockCommentRe);
         if (!consumed) return false;
     }
 
     return stream.atEnd();
+}
+
+export function parseGreyspace(string) {
+    if (string === undefined || string === null)
+        throw new Error("passed undefined or null to isGreyspace");
+
+    let parsedNodes = [];
+
+    let stream = new InputStream(string);
+
+    while (!stream.atEnd()) {
+        if (stream.consume(whitespaceRe)) {
+            parsedNodes.push({ type: "whitespace", value: stream.consumed });
+        } else if (stream.consume(lineCommentRe)) {
+            parsedNodes.push({ type: "lineComment", value: stream.consumed });
+        } else if (stream.consume(blockCommentRe)) {
+            parsedNodes.push({ type: "blockComment", value: stream.consumed });
+        } else if (stream.consume(shebangRe)) {
+            parsedNodes.push({ type: "shebang", value: stream.consumed });
+        } else {
+            // Takes care of assertion that string is greyspace
+            throw new Error("string is not greyspace");
+        }
+    }
+
+    return parsedNodes;
 }
 
 export function log(...messages) {
