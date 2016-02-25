02e33b6cbfc5d058882f6499997cb883a67b6be0
Mark Simulacrum <mark.simulacrum@gmail.com>
2016-02-25 10:15:33 -0700

    Add tests for #6 and #5 (both pass).

--- tests/test-files/es2016-features/input.js
+++ tests/test-files/es2016-features/input.js
@@ -3,3 +3,16 @@ import "test";
 async function test() {
     await Promise.resolve(foobar);
 }
+
+async function Test(take, callback) {
+}
+
+let resolver;
+let promise = new Promise((resolve) => {
+    resolver = resolve;
+});
+
+/*
+ * test
+ * test
+ */
